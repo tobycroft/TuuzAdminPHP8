0417579cd8f48536466453c66116b4ffda8af2eb
tuuz <silverhawk@foxmail.com>
2023-11-15 16:10:09 +0800

    TuuzPC

--- app/BaseController.php
+++ app/BaseController.php
@@ -53,7 +53,7 @@ abstract class BaseController
         $this->app = $app;
         $this->request = $this->app->request;
         $config = Db::name('dp_admin_config')->column('value', 'name');
-        Config::set($config, 'tp');
+        Config::set($config, 'tp.');
         // 控制器初始化
         $this->initialize();
     }

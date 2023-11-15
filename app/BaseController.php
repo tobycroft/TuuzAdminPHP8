a54e59f1b8ace6a6dd09d3d96b3f7bc7ea47d5e0
tuuz <silverhawk@foxmail.com>
2023-11-15 16:02:59 +0800

    TuuzPC

--- app/BaseController.php
+++ app/BaseController.php
@@ -49,11 +49,10 @@ abstract class BaseController
      */
     public function __construct(App $app)
     {
-        $this->app = $app;
-        $this->request = $this->app->request;
-
         $config = Db::name('dp_admin_config')->column('value', 'name');
         Config::set($config, 'tp');
+        $this->app = $app;
+        $this->request = $this->app->request;
         // 控制器初始化
         $this->initialize();
     }

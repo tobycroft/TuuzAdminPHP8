024db90b992e483c5955e25cdceb9b580aeb7acc
tuuz <silverhawk@foxmail.com>
2023-11-15 14:41:49 +0800

    TuuzPC

--- app/Jump.php
+++ app/Jump.php
@@ -11,7 +11,6 @@
  *     }
  * }
  */
-
 namespace app;
 
 use think\Container;
@@ -30,11 +29,11 @@ trait Jump
     /**
      * 操作成功跳转的快捷方法
      * @access protected
-     * @param mixed $msg 提示信息
-     * @param string $url 跳转的URL地址
-     * @param mixed $data 返回的数据
-     * @param integer $wait 跳转等待时间
-     * @param array $header 发送的Header信息
+     * @param  mixed     $msg 提示信息
+     * @param  string    $url 跳转的URL地址
+     * @param  mixed     $data 返回的数据
+     * @param  integer   $wait 跳转等待时间
+     * @param  array     $header 发送的Header信息
      * @return void
      */
     protected function success($msg = '', $url = null, $data = '', $wait = 3, array $header = [])
@@ -47,9 +46,9 @@ trait Jump
 
         $result = [
             'code' => 1,
-            'msg' => $msg,
+            'msg'  => $msg,
             'data' => $data,
-            'url' => $url,
+            'url'  => $url,
             'wait' => $wait,
         ];
 
@@ -67,18 +66,16 @@ trait Jump
     /**
      * 操作错误跳转的快捷方法
      * @access protected
-     * @param mixed $msg 提示信息
-     * @param string $url 跳转的URL地址
-     * @param mixed $data 返回的数据
-     * @param integer $wait 跳转等待时间
-     * @param array $header 发送的Header信息
+     * @param  mixed     $msg 提示信息
+     * @param  string    $url 跳转的URL地址
+     * @param  mixed     $data 返回的数据
+     * @param  integer   $wait 跳转等待时间
+     * @param  array     $header 发送的Header信息
      * @return void
      */
     protected function error($msg = '', $url = null, $data = '', $wait = 3, array $header = [])
     {
         $type = $this->getResponseType();
-        var_dump($type);
-
         if (is_null($url)) {
             $url = $this->app['request']->isAjax() ? '' : 'javascript:history.back(-1);';
         } elseif ('' !== $url) {
@@ -87,9 +84,9 @@ trait Jump
 
         $result = [
             'code' => 0,
-            'msg' => $msg,
+            'msg'  => $msg,
             'data' => $data,
-            'url' => $url,
+            'url'  => $url,
             'wait' => $wait,
         ];
 
@@ -105,23 +102,23 @@ trait Jump
     /**
      * 返回封装后的API数据到客户端
      * @access protected
-     * @param mixed $data 要返回的数据
-     * @param integer $code 返回的code
-     * @param mixed $msg 提示信息
-     * @param string $type 返回数据格式
-     * @param array $header 发送的Header信息
+     * @param  mixed     $data 要返回的数据
+     * @param  integer   $code 返回的code
+     * @param  mixed     $msg 提示信息
+     * @param  string    $type 返回数据格式
+     * @param  array     $header 发送的Header信息
      * @return void
      */
     protected function result($data, $code = 0, $msg = '', $type = '', array $header = [])
     {
         $result = [
             'code' => $code,
-            'msg' => $msg,
+            'msg'  => $msg,
             'time' => time(),
             'data' => $data,
         ];
 
-        $type = $type ?: $this->getResponseType();
+        $type     = $type ?: $this->getResponseType();
         $response = Response::create($result, $type)->header($header);
 
         throw new HttpResponseException($response);
@@ -130,10 +127,10 @@ trait Jump
     /**
      * URL重定向
      * @access protected
-     * @param string $url 跳转的URL表达式
-     * @param array|integer $params 其它URL参数
-     * @param integer $code http code
-     * @param array $with 隐式传参
+     * @param  string         $url 跳转的URL表达式
+     * @param  array|integer  $params 其它URL参数
+     * @param  integer        $code http code
+     * @param  array          $with 隐式传参
      * @return void
      */
     protected function redirect($url, $params = [], $code = 302, $with = [])
@@ -141,7 +138,7 @@ trait Jump
         $response = new Redirect($url);
 
         if (is_integer($params)) {
-            $code = $params;
+            $code   = $params;
             $params = [];
         }
 
@@ -162,9 +159,10 @@ trait Jump
         }
 
         $isAjax = $this->app['request']->isAjax();
-//        $config = $this->app['config'];
+        $config = $this->app['config'];
+
         return $isAjax
-            ? 'json'
-            : 'html';
+        ? $config->get('default_ajax_return')
+        : $config->get('default_return_type');
     }
 }

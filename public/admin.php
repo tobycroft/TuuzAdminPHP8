d47519cf2dd7687f6f7afff5257d79d3da70a3fd
火线兔 <silverhawk@foxmail.com>
2026-05-09 12:17:06 +0800

    TuuzMac

---
+++ public/admin.php
@@ -0,0 +1,43 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2019 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// [ 应用入口文件 ]
+namespace think;
+
+// [ PHP版本检查 ]
+header('Content-type: text/html; charset=utf-8');
+if (version_compare(PHP_VERSION, '8.0', '<')) {
+    die('PHP版本过低，最少需要PHP8.0，请升级PHP版本！');
+}
+
+// 定义后台入口文件
+define('ADMIN_FILE', 'admin.php');
+
+// 定义入口为admin
+define('ENTRANCE', 'admin');
+
+// 加载基础文件
+require __DIR__ . '/../vendor/autoload.php';
+
+// 检查是否安装
+if (!is_file('../data/install.lock')) {
+    // 执行安装模块
+    $http = (new App())->http;
+    $response = $http->name('install')->run();
+    $response->send();
+    $http->end($response);
+} else {
+    // 执行应用并响应
+    $http = (new App())->http;
+    $response = $http->run();
+    $response->send();
+    $http->end($response);
+}

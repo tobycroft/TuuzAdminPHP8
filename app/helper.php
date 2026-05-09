0e4d9e1b3840cf687b08fbc7ad06b714afb3cf46
火线兔 <silverhawk@foxmail.com>
2026-05-09 13:19:54 +0800

    TuuzMac

--- app/helper.php
+++ app/helper.php
@@ -66,7 +66,7 @@ if (!function_exists('action')) {
 
 if (!function_exists('app')) {
     /**
-     * 快速获取容器中的实例 支持依赖注入
+     * 快速获取容器中的实例 支持依赖注入（适配ThinkPHP 8）
      * @param string $name 类名或标识 默认获取当前应用实例
      * @param array $args 参数
      * @param bool $newInstance 是否每次创建新的实例
@@ -74,7 +74,7 @@ if (!function_exists('app')) {
      */
     function app($name = 'think\App', $args = [], $newInstance = false)
     {
-        return Container::get($name, $args, $newInstance);
+        return Container::getInstance()->get($name, $args, $newInstance);
     }
 }
 

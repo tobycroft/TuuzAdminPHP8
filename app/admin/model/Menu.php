ebb6c46d0ec2802980bcee65e19e0dbe3757a069
火线兔 <silverhawk@foxmail.com>
2026-05-09 15:21:00 +0800

    TuuzMac

--- app/admin/model/Menu.php
+++ app/admin/model/Menu.php
@@ -229,8 +229,7 @@ class Menu extends Model
                 ['pid', '<>', 0],
                 ['url_value', '=', strtolower($model . '/' . trim(preg_replace("/[A-Z]/", "_\\0", $controller), "_") . '/' . $action)]
             ];
-            var_dump(self::where($map)
-                ->value('id'));
+            var_dump(strtolower($model . '/' . trim(preg_replace('/[A-Z]/', "_\\0", $controller), '_') . '/' . $action));
             // 当前操作对应的节点ID
             $curr_id = $id == '' ? self::where($map)
                 ->value('id') : $id;

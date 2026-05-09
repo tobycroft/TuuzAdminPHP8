9acdf167ead01865f8965297084bbd222e22d19e
Tuuz <2539@tuuz.cn>
2026-05-09 20:51:45 +0800

    TuuzPC

--- app/admin/builder/form/Builder.php
+++ app/admin/builder/form/Builder.php
@@ -67,7 +67,7 @@ class Builder extends ZBuilder
      */
     public function initialize()
     {
-        $this->_template = 'common/builder/form/layout';
+        $this->_template = Env::get('app_path') . 'admin/builder/form/layout.html';
         $this->_vars['post_url'] = $this->request->url(true);
         $token_name = config_old('zbuilder.form_token_name') ?: '__token__';
         $this->_vars['_token_name'] = $token_name;

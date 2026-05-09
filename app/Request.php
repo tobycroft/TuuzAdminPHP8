9c1a95f42e3ff0a38314aa4df11d5bb2bec4b871
Tuuz <2539@tuuz.cn>
2026-05-09 20:32:08 +0800

    TuuzPC

--- app/Request.php
+++ app/Request.php
@@ -4,5 +4,14 @@ namespace app;
 // 应用请求对象类
 class Request extends \think\Request
 {
-
+    /**
+     * 生成表单令牌
+     * @param string $name 令牌名称
+     * @param string $type 令牌生成方法
+     * @return string
+     */
+    public function token($name = '__token__', $type = 'md5')
+    {
+        return $this->buildToken($name, $type);
+    }
 }

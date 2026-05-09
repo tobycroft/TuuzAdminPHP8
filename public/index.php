77876a33f0ae4080f887d5b268f26ae3d03aa9a1
火线兔 <silverhawk@foxmail.com>
2026-05-09 10:33:34 +0800

    TuuzMac

--- public/index.php
+++ public/index.php
@@ -12,13 +12,31 @@
 // [ 应用入口文件 ]
 namespace think;
 
+// [ PHP版本检查 ]
+header("Content-type: text/html; charset=utf-8");
+if (version_compare(PHP_VERSION, '8.0', '<')) {
+    die('PHP版本过低，最少需要PHP8.0，请升级PHP版本！');
+}
+
+// 定义后台入口文件
+define('ADMIN_FILE', 'admin.php');
+
+// 加载基础文件
 require __DIR__ . '/../vendor/autoload.php';
 
-// 执行HTTP应用并响应
-$http = (new App())->http;
+// 支持事先使用静态方法设置Request对象和Config对象
 
-$response = $http->run();
-
-$response->send();
-
-$http->end($response);
+// 检查是否安装
+if (!is_file('../data/install.lock')) {
+    // 执行安装模块
+    $http = (new App())->http;
+    $response = $http->name('install')->run();
+    $response->send();
+    $http->end($response);
+} else {
+    // 执行应用并响应
+    $http = (new App())->http;
+    $response = $http->run();
+    $response->send();
+    $http->end($response);
+}

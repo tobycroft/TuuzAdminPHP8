c4063d1c09b20a9d70565fbad732d8177e00294d
火线兔 <silverhawk@foxmail.com>
2026-05-09 11:00:35 +0800

    TuuzMac

---
+++ app/admin/controller/Test.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\admin\controller;
+
+use app\common\controller\Common;
+
+/**
+ * ie提示页面控制器
+ * @package app\admin\controller
+ */
+class Test extends Common
+{
+    /**
+     * 显示ie提示
+     * @return mixed
+     */
+    public function index()
+    {
+      return "test";
+    }
+}

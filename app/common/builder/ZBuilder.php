a1e003e08c255ff8eea850e67fbb87b5875eb784
火线兔 <silverhawk@foxmail.com>
2026-05-09 17:11:07 +0800

    TuuzMac

---
+++ app/common/builder/ZBuilder.php
@@ -0,0 +1,86 @@
+<?php
+
+
+namespace app\admin\builder;
+
+use app\common\controller\Common;
+use think\Exception;
+
+/**
+ * 构建器
+ * @package app\common\builder
+ */
+class ZBuilder extends Common
+{
+    /**
+     * @var array 构建器数组
+     */
+    protected static $builder = [];
+
+    /**
+     * @var array 模板参数变量
+     */
+    protected static $vars = [];
+
+    /**
+     * @var string 动作
+     */
+    protected static $action = '';
+
+    /**
+     * 初始化
+     */
+    public function initialize()
+    {
+    }
+
+    /**
+     * 创建各种builder的入口
+     * @param string $type 构建器名称，'Form', 'Table', 'View' 或其他自定义构建器
+     * @param string $action 动作
+     * @return table\Builder|form\Builder|aside\Builder
+     * @throws Exception
+     */
+    public static function make($type = '', $action = '')
+    {
+        if ($type == '') {
+            throw new Exception('未指定构建器名称', 8001);
+        } else {
+            $type = strtolower($type);
+        }
+
+        // 构造器类路径 - 先检查 admin 目录
+        $class = '\\app\\admin\\builder\\' . $type . '\\Builder';
+        
+        // 如果 admin 目录下不存在，检查 common 目录
+        if (!class_exists($class)) {
+            $class = '\\app\\common\\builder\\' . $type . '\\Builder';
+        }
+        
+        if (!class_exists($class)) {
+            throw new Exception($type . '构建器不存在', 8002);
+        }
+
+        if ($action != '') {
+            static::$action = $action;
+        } else {
+            static::$action = '';
+        }
+
+        // ✅ 这一行修复：传入 app() 参数
+        return new $class(app());
+    }
+
+    /**
+     * 加载模板输出
+     * @param string $template 模板文件名
+     * @param array $vars 模板输出变量
+     * @param array $config 模板参数
+     * @return mixed
+     */
+    public function fetch($template = '', $vars = [], $config = [])
+    {
+        $vars = array_merge($vars, self::$vars);
+        return parent::fetch($template, $vars, $config);
+    }
+}

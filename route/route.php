4c88fc3c56f657e4f33e3c929639d3e8880735a4
火线兔 <silverhawk@foxmail.com>
2026-05-09 10:57:38 +0800

    TuuzMac

--- route/route.php
+++ route/route.php
@@ -1,7 +1,7 @@
 <?php
 
 \think\facade\Route::any(':module/:controller/:function',
-    '\app\:version\:module\controller\:controller@:function')->before(function () {
+    '\app\:module\controller\:controller@:function')->before(function () {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Max-Age: 86400');
     header('Access-Control-Allow-Credentials: true');

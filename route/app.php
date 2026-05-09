52c8b803635f3cd3fa5eb060865a3854e558d47f
火线兔 <silverhawk@foxmail.com>
2026-05-09 10:43:40 +0800

    TuuzMac

--- route/app.php
+++ route/app.php
@@ -9,3 +9,17 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
+
+use think\facade\Route;
+
+// 后台管理路由 - admin 应用
+Route::rule('admin', 'admin/index/index');
+Route::rule('admin/:controller', 'admin/:controller/index');
+Route::rule('admin/:controller/:action', 'admin/:controller/:action');
+Route::rule('admin/:controller/:action/:id', 'admin/:controller/:action');
+
+// 前台应用路由 - index 应用
+Route::rule('/', 'index/index/index');
+Route::rule(':controller', 'index/:controller/index');
+Route::rule(':controller/:action', 'index/:controller/:action');
+Route::rule(':controller/:action/:id', 'index/:controller/:action');
